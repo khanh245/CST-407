c7b0d8fa97025eb0659b6d61998a32c459bcc771
Khanh Nguyen <knultimate@live.com>
2014-07-07 19:05:45 -0700

    Updated Hw2
    Layout French Portrait

--- LayoutResources/src/oit/edu/hw2/part1/OpinionActivity.java
+++ LayoutResources/src/oit/edu/hw2/part1/OpinionActivity.java
@@ -1,7 +1,14 @@
 package oit.edu.hw2.part1;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class OpinionActivity extends Activity {
 	
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_opinion);
+	}
 }
