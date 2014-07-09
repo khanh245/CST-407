09f8ca57ee6917a38912d34a7d09b8c811b8bfcd
Khanh Nguyen <knultimate@live.com>
2014-07-08 19:21:17 -0700

    Updated HW2 Part 3
    Started PersistentPreferences

---
+++ PersistentPreferences/src/oit/edu/hw2/part3/GenieActivity.java
@@ -0,0 +1,13 @@
+package oit.edu.hw2.part3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GenieActivity extends Activity{
+	
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_genie);
+	}
+}
