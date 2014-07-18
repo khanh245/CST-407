62a03090898652ae4839b47d2dce62854696970a
Khanh Nguyen <knultimate@live.com>
2014-07-18 11:55:30 -0700

    Aurec GPS Service
    Updated GPS Data Service

--- PinpointWaldo/src/oit/edu/pinpointwaldo/services/GPSDataService.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/services/GPSDataService.java
@@ -1,18 +1,38 @@
 package oit.edu.pinpointwaldo.services;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
 
-public class GPSDataService extends IntentService {
+public class GPSDataService extends Service implements LocationListener {
 
-	public GPSDataService(String name) {
-		super(name);
+	public GPSDataService() {
+		super();
 		
 	}
 
 	@Override
-	protected void onHandleIntent(Intent intent) {
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) { }
+	@Override
+	public void onProviderDisabled(String provider) { }
 }
