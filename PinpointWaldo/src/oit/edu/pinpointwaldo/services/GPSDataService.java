79a73229fe471a873b992f44d6ab3adaf22799eb
Khanh Nguyen <knultimate@live.com>
2014-07-19 15:20:47 -0700

    Pinpoint Waldo
    Updated GPSDataService

--- PinpointWaldo/src/oit/edu/pinpointwaldo/services/GPSDataService.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/services/GPSDataService.java
@@ -4,11 +4,14 @@ import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 
 public class GPSDataService extends Service implements LocationListener {
 
+	private LocationManager mManager = null;
+	
 	/// Service Implementation
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -17,12 +20,12 @@ public class GPSDataService extends Service implements LocationListener {
 
 	@Override
 	public void onCreate() {
-		
+		super.onCreate();
 	}
 	
 	@Override
 	public void onDestroy() {
-		
+		super.onDestroy();
 	}
 	
 	/// Location Listener Implementation
@@ -33,11 +36,7 @@ public class GPSDataService extends Service implements LocationListener {
 	}
 
 	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void onStatusChanged(String provider, int status, Bundle extras) { }
 	@Override
 	public void onProviderEnabled(String provider) { }
 	@Override
