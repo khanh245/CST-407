0980b1fc6a7e163d0bc390a0488c9b7946b61e03
Khanh Nguyen <knultimate@live.com>
2014-07-21 13:12:23 -0700

    Pinpoint Waldo
    Working Service with BroadcastReceiver

--- PinpointWaldo/src/oit/edu/pinpointwaldo/MainActivity.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/MainActivity.java
@@ -1,19 +1,71 @@
 package oit.edu.pinpointwaldo;
 
+import oit.edu.pinpointwaldo.services.GPSDataService;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
+	public static final String GPS_LOCATION = "GPS_LOCATION";
 	private GoogleMap mMap = null;
-	
+	private BroadcastReceiver mReceiver = null;
+
+	private void requestGPSService() {
+		IntentFilter filter = new IntentFilter("android.intent.action.MAIN");
+		mReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				Location loc = null;
+				loc = (Location) intent.getExtras().get(GPS_LOCATION);
+
+				if (loc != null) {
+					double lat = loc.getLatitude();
+					double lon = loc.getLongitude();
+					Log.d("Location: ", lat + ", " + lon);
+					LatLng coord = new LatLng(lat, lon);
+					
+					mMap.addMarker(new MarkerOptions().position(coord).title("Waldo!"));
+					mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 17));
+				}
+			}
+		};
+
+		this.registerReceiver(mReceiver, filter);
+		this.startService(new Intent(this, GPSDataService.class));
+	}
+
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
-		mMap.setMyLocationEnabled(true);
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		mMap.getUiSettings().setAllGesturesEnabled(true);
+		
+		requestGPSService();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		requestGPSService();		
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		this.unregisterReceiver(mReceiver);
 	}
 }
--- PinpointWaldo/src/oit/edu/pinpointwaldo/services/GPSDataService.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/services/GPSDataService.java
@@ -1,20 +1,38 @@
 package oit.edu.pinpointwaldo.services;
 
+import oit.edu.pinpointwaldo.MainActivity;
 import android.app.Service;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class GPSDataService extends Service implements LocationListener {
 
 	private LocationManager mManager = null;
-	
+	private String provider = null;
+
 	private void initialize() {
 		mManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+		Location loc = mManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		broadcast(loc);
+		provider = mManager.getBestProvider(new Criteria(), false);
+		mManager.requestLocationUpdates(provider, 1000, 1, this);		
+	}
+	
+	private void broadcast(Location loc) {
+		Intent i = new Intent("android.intent.action.MAIN").putExtra(MainActivity.GPS_LOCATION, loc);
+		this.sendBroadcast(i);
+		this.stopSelf();
+		Log.d("WALDO_SERVICE", "Service's stopped...");
 	}
 	
 	/// Service Implementation
@@ -26,40 +44,50 @@ public class GPSDataService extends Service implements LocationListener {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		initialize();
 		
+		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+        if( status != ConnectionResult.SUCCESS) {
+            Log.e("LOCATION_SERVICE", "API NOT AVAILABLE");
+            stopSelf();
+        }
+        initialize();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		if(mManager != null)
+		if (mManager != null)
 			mManager.removeUpdates(this);
 	}
-	
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
+		Log.d("WALDO_SERVICE", "Service's started...");
+		initialize();
+		
 		return START_NOT_STICKY;
 	}
-	
+
 	/// Location Listener Implementation
 	@Override
 	public void onLocationChanged(Location location) {
-		// TODO Auto-generated method stub
-		
+		Log.d("WALDO_SERVICE", "Location changed");
+		broadcast(location);
 	}
 
 	@Override
 	public void onStatusChanged(final String provider, final int status, final Bundle extras) { }
-	
+
 	@Override
 	public void onProviderEnabled(String provider) {
-		Toast.makeText(getBaseContext(), provider + " enabled.", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getBaseContext(), provider + " enabled.",
+				Toast.LENGTH_SHORT).show();
 	}
-	
+
 	@Override
 	public void onProviderDisabled(String provider) {
-		Toast.makeText(getBaseContext(), provider + " disabled.", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getBaseContext(), provider + " disabled.",
+				Toast.LENGTH_SHORT).show();
 	}
 }
