874a5738f53f4b6d2b619c150bdfde8f65edd2fb
Khanh Nguyen <knultimate@live.com>
2014-07-26 16:13:28 -0700

    Changed to LocalBroadcastManager

--- PinpointWaldo/src/oit/edu/pinpointwaldo/WaldoMapFragment.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/WaldoMapFragment.java
@@ -16,6 +16,7 @@ import android.content.IntentFilter;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,8 +42,9 @@ public class WaldoMapFragment extends Fragment {
 
 				if (mMarker != null)
 					mMarker.remove();
-				
-				mMarker = mMap.addMarker(new MarkerOptions().position(coord).title("Waldo!"));
+
+				mMarker = mMap.addMarker(new MarkerOptions().position(coord)
+						.title("Waldo!"));
 				mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 17));
 			}
 		}
@@ -50,25 +52,25 @@ public class WaldoMapFragment extends Fragment {
 
 	private void requestGPSService() {
 		IntentFilter filter = new IntentFilter("android.intent.action.MAIN");
-		getActivity().registerReceiver(mReceiver, filter);
+		LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mReceiver, filter);
 		getActivity().startService(new Intent(getActivity(), GPSDataService.class));
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		SupportMapFragment smf = (SupportMapFragment)getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+		SupportMapFragment smf = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
 		if (smf != null) {
 			mMap = smf.getMap();
 		}
 		mMap.getUiSettings().setAllGesturesEnabled(true);
 		requestGPSService();
-		
+
 		return inflater.inflate(R.layout.fragment_map, container, false);
-	}	
-	
+	}
+
 	@Override
 	public void onDestroyView() {
-		getActivity().unregisterReceiver(mReceiver);
+		LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mReceiver);
 		super.onDestroyView();
 	}
 }
