9e999f6508cdab59ad185aa9a3bf696fa35b3d2f
Khanh Nguyen <knultimate@live.com>
2014-07-27 23:19:47 -0700

    ActionBar with Search and Settings
    Uses SettingView, Searchable, and SearchInfo with singleTop launchMode.
    Handles Intent with Search IntentFilter. (onNewIntent, to capture Search
    intent)

--- PinpointWaldo/src/oit/edu/pinpointwaldo/MainActivity.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/MainActivity.java
@@ -1,12 +1,48 @@
 package oit.edu.pinpointwaldo;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		handleIntent(getIntent());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+			SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+			searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+		}
+
+		return true;
+	}
+
+	@Override
+	protected void onNewIntent(Intent i)
+	{
+		handleIntent(i);
+	}
+	
+	private void handleIntent(Intent i) {
+		if (Intent.ACTION_SEARCH.equals(i.getAction())) {
+			String query = i.getStringExtra(SearchManager.QUERY);
+			Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+		}
 	}
 }
