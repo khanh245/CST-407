8f6d50bbf195f1a783f96047b90e54aad4f187e4
Khanh Nguyen <knultimate@live.com>
2014-07-28 18:10:34 -0700

    Added Navigation Drawer with AppIcon Drawer Toggle

--- PinpointWaldo/src/oit/edu/pinpointwaldo/MainActivity.java
+++ PinpointWaldo/src/oit/edu/pinpointwaldo/MainActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -19,18 +22,23 @@ public class MainActivity extends Activity {
 	private ActionBarDrawerToggle mDrawerToggle = null;
 	private CharSequence mTitle = null;
 	
+	private ListView mDrawerList = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        
 		mTitle = getTitle();
 		mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.list_drawermenu);
+		
 		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, R.drawable.ic_drawer, R.string.drawer_open_desc, R.string.drawer_close_desc) {
 
 			public void onDrawerClosed (View view) {
 				super.onDrawerClosed(view);
-				getActionBar().setTitle(mTitle);
 				invalidateOptionsMenu();
 			}
 			
@@ -56,9 +64,32 @@ public class MainActivity extends Activity {
 			searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 		}
 
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (mDrawer != null)
+        	mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (mDrawer != null)
+        	mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+          }
+        
+        return super.onOptionsItemSelected(item);
+	};
+	
 	@Override
 	protected void onNewIntent(Intent i)
 	{
