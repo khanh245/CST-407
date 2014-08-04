45adc19aad5b382296dda34c2bd47553637f6d56
Khanh Nguyen <knultimate@live.com>
2014-08-04 12:20:18 -0700

    Updated HW 5
    Main Activity Layout

--- SimpleService/src/com/example/simpleservice/MainActivity.java
+++ SimpleService/src/com/example/simpleservice/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
+			
 		}
 	};
 
