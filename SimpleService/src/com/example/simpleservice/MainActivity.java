a82b24e4007cf1e9252ff3f351d88372d5f6293b
Khanh Nguyen <knultimate@live.com>
2014-08-04 20:39:52 -0700

    Update HW5
    Added button handle

--- SimpleService/src/com/example/simpleservice/MainActivity.java
+++ SimpleService/src/com/example/simpleservice/MainActivity.java
@@ -5,9 +5,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button mStart = null;
+	private Button mClear = null;
+	
 	private BroadcastReceiver mLocalRcvr = new BroadcastReceiver() {
 
 		@Override
@@ -21,10 +26,17 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mStart = (Button) findViewById(R.id.btnStart);
+		mClear = (Button) findViewById(R.id.btnClear);
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
+	}
+	
+	public void ButtonClicked(View view) {
+		
 	}
 }
